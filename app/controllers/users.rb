d1b2477eaa371779d2b8a118e946e74c4b176bf3
Sherianne Bolling <sheriannebolling@gmail.com>
2016-12-01 16:49:27 -0500

    Add button to get exercise

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -6,6 +6,7 @@ post '/users' do
   @user = User.new(params[:user])
   if @user.save
     session[:user_id] = @user.id
+    @exercises = @user.exercises.all 
     redirect "users/#{@user.id}"
   else
     @errors = @user.errors.full_messages
@@ -15,6 +16,6 @@ end
 
 get '/users/:id' do
   @user = User.find_by(id: params[:id])
-  @exercises = @user.exercises.all 
+  @exercises = @user.exercises 
   erb :'/users/show'
 end
