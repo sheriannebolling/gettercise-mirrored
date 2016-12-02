848aa09710404369579a24cf02638f723972c9aa
Sherianne Bolling <sheriannebolling@gmail.com>
2016-12-02 12:02:31 -0500

    make ajax and api call work

--- api.rb
+++ api.rb
@@ -1,7 +1,7 @@
 require 'pry'
 require 'JSON'
 
-query = `curl http://wger.de/api/v2/exercise/?language=2`
+var query = `curl http://wger.de/api/v2/exercise/?language=2`;
 
 result = JSON.parse(query)
 
@@ -11,5 +11,3 @@ random = 1+ rand(40)
 description = result["results"][random]["description"]
 
 # binding.pry
-
-p result 
